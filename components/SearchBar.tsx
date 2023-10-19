3dbd480ffb9f4d685e23ae06cf0ed1ab921f5fa4
Eetu Eskelinen <eetuesk@gmail.com>
2023-10-19 11:56:06 +0300

    Add DaisyUI, improve search logic

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,21 +1,42 @@
 import React from 'react';
 import { useState } from 'react';
+import { useSession } from 'next-auth/react';
 
 interface SearchComponentProps {
-  onSearch: (query: string, searchType: string) => void;
+  // onSearch: (query: string, searchType: string) => void;
+  // search results
+  onSelectItem?: (item: any) => void;
 }
 
-const SearchBar: React.FC<SearchComponentProps> = ({ onSearch }) => {
+const SearchBar: React.FC<SearchComponentProps> = ({ onSelectItem }) => {
+  const { data: session, status } = useSession();
   const [query, setQuery] = useState<string>('');
   const [searchType, setSearchType] = useState<string>('track'); // Default to searching for tracks
+  const [searchResults, setSearchResults] = useState<any[]>([]);
 
-  const handleSearch = () => {
+  const handleSearch = async () => {
     if (query.trim() === '') {
       // Don't search if the query is empty
       return;
     }
 
-    onSearch(query, searchType);
+    if (!session) return;
+    console.log(query, searchType);
+
+    // Fetch results from /api/search with GET request. Then handle errors.
+    const res = await fetch(`/api/search?query=${query}&type=${searchType}`);
+    const data = await res.json();
+    if (!res.ok) {
+      console.error(data);
+      return;
+    }
+    console.log(data);
+
+    // get all the results from the items array. they can either be under tracks or artists
+    const results = data.tracks?.items || data.artists?.items || [];
+    setSearchResults(results);
+    console.log('search results: ', results);
+    return data;
   };
 
   return (
@@ -23,10 +44,14 @@ const SearchBar: React.FC<SearchComponentProps> = ({ onSearch }) => {
       <div>
         <input
           type="text"
-          placeholder="Enter your search query"
+          placeholder={
+            searchType === 'track'
+              ? 'Search for a track'
+              : 'Search for an artist'
+          }
           value={query}
           onChange={(e) => setQuery(e.target.value)}
-          className="border-2 border-gray-300 rounded-lg p-2 text-black"
+          className="input input-bordered w-full max-w-xs"
         />
       </div>
       <div>
@@ -49,7 +74,27 @@ const SearchBar: React.FC<SearchComponentProps> = ({ onSearch }) => {
           Artist
         </label>
       </div>
-      <button onClick={handleSearch}>Search</button>
+      <button className="btn" onClick={handleSearch}>
+        Search
+      </button>
+      {searchResults.length > 0 && (
+        <div className="grid grid-cols-2 gap-4">
+          {searchResults.map((item) => (
+            <div
+              key={item.id}
+              className="flex flex-col items-center justify-center"
+            >
+              <p className="text-center">{item.name}</p>
+              <button
+                className="btn btn-sm"
+                onClick={() => onSelectItem && onSelectItem(item)}
+              >
+                Select
+              </button>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,18 @@ import { Inter } from 'next/font/google';
 import LoginButton from '@/components/login-btn';
 import SearchBar from '@/components/SearchBar';
 import { useSession } from 'next-auth/react';
+import { useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   const { data: session, status } = useSession();
+
+  const [selectedSongs, setSelectedSongs] = useState<any[]>([]);
+
+  const handleSelectItem = (item: any) => {
+    setSelectedSongs([...selectedSongs, item]);
+  };
 
   const handleSearch = async (query: string, searchType: string) => {
     if (!session) return;
@@ -29,7 +36,13 @@ export default function Home() {
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <LoginButton />
-      <SearchBar onSearch={handleSearch} />
+      <SearchBar onSelectItem={handleSelectItem} />
+      {selectedSongs.map((song) => (
+        <div key={song.id}>
+          <h2>{song.name}</h2>
+          <p>{song.artists[0].name}</p>
+        </div>
+      ))}
 
       {/* <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
         <Image
