57bc58a2bfa9a269980c7daf983130a3780ace95
Eetu Eskelinen <eetuesk@gmail.com>
2023-10-28 12:26:55 +0300

    Update footer text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,17 @@ export const Footer: React.FC = () => {
   return (
     <footer className="footer footer-center p-4 bg-base-300 text-base-content mt-auto">
       <aside className="flex gap-6">
+        <p>
+          Created by{' '}
+          <a
+            href="https://eetueskelinen.com"
+            className="hover:link-accent hover:underline hover:decoration-wavy"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Eetu Eskelinen
+          </a>
+        </p>
         <p>Created with 🎶 by Eetu Eskelinen</p>
         {/* <p>&copy; {new Date().getFullYear()} - All rights reserved </p> */}
       </aside>
